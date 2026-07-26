dbe4cdbdfe016422d7e0341e6b10327ff9c7329a
Tebio <116451450+Tebio@users.noreply.github.com>
2026-07-26 19:10:02 +0800

    feat(rust): main entry (borderless transparent viewport)

---
+++ anime-widget-rs/src/main.rs
@@ -0,0 +1,45 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod agedm;
+mod app;
+mod feed;
+mod models;
+mod settings;
+
+use app::AnimeWidgetApp;
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let settings = settings::WidgetSettings::load();
+    let w = settings.window_w.unwrap_or(360.0);
+    let h = settings.window_h.unwrap_or(520.0);
+
+    let mut viewport = egui::ViewportBuilder::default()
+        .with_inner_size([w, h])
+        .with_min_inner_size([280.0, 320.0])
+        .with_decorations(false)
+        .with_transparent(true)
+        .with_resizable(true)
+        .with_window_level(if settings.always_on_top {
+            egui::WindowLevel::AlwaysOnTop
+        } else {
+            egui::WindowLevel::Normal
+        })
+        .with_title("Anime Following Widget");
+
+    if let (Some(x), Some(y)) = (settings.window_x, settings.window_y) {
+        viewport = viewport.with_position([x, y]);
+    }
+
+    let options = eframe::NativeOptions {
+        viewport,
+        centered: settings.window_x.is_none(),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Anime Following Widget",
+        options,
+        Box::new(|cc| Ok(Box::new(AnimeWidgetApp::new(cc)))),
+    )
+}
