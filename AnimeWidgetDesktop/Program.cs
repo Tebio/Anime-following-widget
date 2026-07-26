bc73af9f3ef40a16e6386285bd05390b55341a74
Tebio <116451450+Tebio@users.noreply.github.com>
2026-07-26 17:35:40 +0800

    feat: add app entry point

---
+++ AnimeWidgetDesktop/Program.cs
@@ -0,0 +1,11 @@
+namespace AnimeWidgetDesktop;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
